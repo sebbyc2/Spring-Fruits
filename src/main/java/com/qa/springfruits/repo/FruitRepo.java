d9c222b1d8adf8e81605689f57692fb271ea3977
sebby <sebbyc2@gmail.com>
2022-01-31 17:39:19 +0000

    added @Repository annotation to FruitRepo Interface

--- src/main/java/com/qa/springfruits/repo/FruitRepo.java
+++ src/main/java/com/qa/springfruits/repo/FruitRepo.java
@@ -1,9 +1,11 @@
 package com.qa.springfruits.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qa.springfruits.domain.Fruit;
 
+@Repository
 public interface FruitRepo extends JpaRepository<Fruit, Integer>{
 	
 }
